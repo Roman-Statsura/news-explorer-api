34b4424f07da4963b7b257fcba1bc62c479e2167
Roman-Statsura <srs1012@yandex.ru>
2020-09-24 17:43:50 +0400

    created readme

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
